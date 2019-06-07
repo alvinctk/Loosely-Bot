2dafd5a74ffcc5467a4c9cc242520f4fb881e7d7
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-06-06 23:17:00 -0600

    config.py is configuration id for discord channel id and other kgs codes.

---
+++ config.py
@@ -0,0 +1,69 @@
+clyde_command = {
+    "/tableflip": "(╯°□°）╯︵ ┻━┻",
+    "/unflip": "┬─┬ ノ( ゜-゜ノ)",
+    "/shrug": "/¯\_(ツ)_/¯"
+}
+
+addon_command = {
+    "/gudtoltableflip": "(┛ಠ_ಠ)┛彡┻━┻",
+    "/madtableflip": "(ノಠ益ಠ)ノ彡┻━┻",
+    "/twotableflip": "┻━┻︵ (°□°)/ ︵ ┻━┻",
+    "/peopleflip": "(╯°Д°）╯︵/(.□ . )",
+    "/fingerguns": " \╭☞ \╭☞",
+    "/face": "( ͡ ͡° ͜ ʖ ͡ ͡°)"
+}
+
+channels = {
+    'general': '251872733661495297',
+    'Text Channels': '540778800129114122',
+    'life-and-death': '540778905548881921',
+    'opening': '540778949312118813',
+    'middle-game': '540779042161557515',
+    'end-game': '540779075204153344',
+    'tesuji': '540779106350923787',
+    'joseki': '540779153608278017',
+    'Voice Channel': '540779874605072395',
+    'Lemon Game Review': '540780009506209802',
+    'Coyote Game Review': '540780074241359872',
+    'pro-games': '540780260556406785',
+    'Study Group 1': '540780643647488030',
+    'Study Group 2': '540780695857922058',
+    '1-1': '540780880331931668',
+    'Chill at Puff Factory':
+    '540781035244224512',
+    'General Chat': '540781194388832266',
+    'kgs-room-message-board': '540783039425544244',
+    'nsfw': '541352435252461588',
+    'AFK Channel': '541353302848176143',
+    'study-group': '542495807333597235',
+    'tournament': '543202907781333022',
+    "Loosely's Study Group": '543351220253753353',
+    'Music': '543351695283978240',
+    'music-bot': '544215804724510769',
+    'test-bot': '544690050755985410',
+    'kgs-test-bot': '544774431361335307',
+    'kgs': '545726526986125314'
+}
+
+kgs_user_flags_status = {
+    "d": "Account has been deleted",
+    "s": "Idle",
+    "c": "Online"
+}
+kgs_user_flags_long = {
+    "g": "Guest",
+    "c": "Connected (logged in right now)",
+    "d": "Account has been deleted",
+    "s": "User is sleeping (more than 10 minutes since last interaction)",
+    "a": "User has an avatar",
+    "r": "User is a robot.",
+    "TT": "User has won a KGS tournament.",
+    "t": "User has been runner up in a KGS tournament.",
+    "p": "User is currently playing in a game.",
+    "P": "User is currently playing in a KGS tournament game.",
+    "*": "User is a KGS Plus subscriber.",
+    "!": "User is KGS Meijin",
+    "m": "User is on a mobile device",
+    "=": "User can play ranked games",
+    "~": "User plays stronger players far more often that weaker ones."
+}
